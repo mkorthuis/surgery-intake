a72c5489b5c0a8158fbf70f374cb7e52ab956b88
mkorthuis <michael.korthuis@gmail.com>
2018-01-24 17:32:31 -0500

    Removed extraneous field.

--- src/components/registration/page-one.js
+++ src/components/registration/page-one.js
@@ -31,7 +31,6 @@ class PageOne extends Component {
       'procedurePerformed',
       'procedureDate',
       'procedureSite',
-      'procedureSurgeryConsultReason',
       'institutionName',
       'institutionAddressOne',
       'institutionAddressTwo',
