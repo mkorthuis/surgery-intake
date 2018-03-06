c869a31fd900e4358c30a5e6348106f6d3a404ff
mkorthuis <michael.korthuis@gmail.com>
2018-03-06 16:04:43 -0500

    Added a simple thank you submission page.  Needs content.

--- src/components/registration/page-six.js
+++ src/components/registration/page-six.js
@@ -5,12 +5,26 @@ import {
   connect
 } from 'react-redux'
 import {
-  Panel
+  Panel,
+  Grid,
+  Row,
+  Col
 } from 'react-bootstrap'
 
 class PageSix extends Component {
   render() {
-    return (<Panel>Thank you for your submission</Panel>)
+    return (
+      <Grid className="main-grid">
+        <Row>
+          <Col xs={12}>
+            <img src="https://upload.wikimedia.org/wikipedia/commons/a/aa/Lowell_General_Hospital_%28logo%29.jpg" alt="Logo" className="pull-right Company-Logo"/>
+            <h3>Thank you for submitting the form</h3>
+            <p>We look forward to seeing you shortly</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec diam sem, congue in risus in, aliquet varius sapien. Cras interdum malesuada rutrum. Etiam posuere, dolor id euismod ullamcorper, velit urna pretium urna, id mattis odio dui maximus arcu. Phasellus imperdiet, nisi ac iaculis lobortis, nulla orci iaculis massa, et pellentesque ante purus quis tellus. Donec posuere congue erat et feugiat. Curabitur non ante eu mi euismod vehicula at a ligula. In ac arcu erat. Pellentesque at porta libero, vitae euismod eros. Aliquam facilisis mauris mauris, eu viverra neque feugiat eget. Etiam ultrices eros molestie posuere dictum. In malesuada dolor vel viverra auctor. Nulla semper faucibus facilisis.</p>
+          </Col>
+        </Row>
+      </Grid>
+    )
   }
 }
 
